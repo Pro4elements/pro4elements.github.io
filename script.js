69745a41e56dc1f3a73e9814f43c08ef0ed0e86b
Pro4elements <pro4elements@gmail.com>
2025-08-28 09:41:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// Cuando cargue la página, muestra un mensaje en la consola
-console.log("¡Bienvenido a mi página de somatotipos!");
 
-// Ejemplo: hacer que un botón cambie el texto
-function cambiarTexto() {
-  document.getElementById("mensaje").innerText = "¡Texto cambiado con JavaScript!";
-}
